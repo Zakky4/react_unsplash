90c0925477e71fbe015152e181e3410111ebe5b6
zakky <sadaoza@gmail.com>
2024-06-18 08:10:17 +0900

    Layout,Style Changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import useUnsplashImage from './hooks/useUnsplashImage';
-import './App.css';
+import React from "react";
+import useUnsplashImage from "./hooks/useUnsplashImage";
+import "./App.css";
 
 const App: React.FC = () => {
-  const { imageUrl, error, fetchRandomImage } = useUnsplashImage();
+  const { imageUrl, fetchRandomImage } = useUnsplashImage();
 
   return (
     <div className="App">
-      <h1>Unsplashからランダムな画像を取得</h1>
-      {imageUrl && <img src={imageUrl} alt="Random from Unsplash" style={{ width: '500px', height: 'auto' }} />}
-      <br />
+      <h1>Unsplashからランダムに画像を取得</h1>
       <button onClick={fetchRandomImage}>新しい画像を取得</button>
+      <br />
+      <br />
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt="Random from Unsplash"
+          style={{ width: "500px", height: "auto" }}
+        />
+      )}
     </div>
   );
 };
